8cd8fb518a1dda4966da91b66e7f620b7c9dc123
Ivan Moore <ivan@teamoptimization.com>
2024-01-19 15:00:54 +0000

    adds experimental main

--- src/main/java/com/oocode/ExtremeStartupHttpServer.java
+++ src/main/java/com/oocode/ExtremeStartupHttpServer.java
@@ -5,13 +5,32 @@ import io.fusionauth.http.server.HTTPListenerConfiguration;
 import io.fusionauth.http.server.HTTPServer;
 
 import java.io.Writer;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class ExtremeStartupHttpServer {
     private final HTTPServer server;
 
+    public static void main(String[] args) throws Exception {
+        int port = 8123;
+        System.out.println("Starting server for: http://" + myIpAddress() + ":" + port);
+        new ExtremeStartupHttpServer(port).start();
+    }
+
+    private static String myIpAddress() throws UnknownHostException, SocketException {
+        try (final DatagramSocket datagramSocket = new DatagramSocket()) {
+            datagramSocket.connect(InetAddress.getByName("8.8.8.8"), 12345);
+            return datagramSocket.getLocalAddress().getHostAddress();
+        }
+    }
+
     public ExtremeStartupHttpServer(int port) {
         HTTPHandler handler = (req, res) -> {
-            try(Writer writer = res.getWriter()) {
+            String queryString = req.getQueryString();
+            System.out.println("queryString = " + queryString);
+            try (Writer writer = res.getWriter()) {
                 writer.write("42");
             }
         };
